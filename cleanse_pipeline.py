acced8917965b409ad8e1ef5587a9fca572ee49c
Vitor Eulalio Reis <vitor.reis@estudante.ufscar.br>
2024-02-09 13:02:22 -0300

    pipelines added

---
+++ cleanse_pipeline.py
@@ -0,0 +1,242 @@
+import re
+import copy
+import spacy
+import pandas as pd
+import language_tool_python
+from pathlib import Path
+from docx import Document
+from docx.shared import Cm
+from itertools import product
+from functools import partial
+from multiprocessing import Pool
+from generate_rels import *
+
+tool = language_tool_python.LanguageTool('en-US')  # use a local server (automatically set up), language English
+
+# Load the English language model
+nlp = spacy.load("en_core_web_sm")
+
+def starts_with_figure(row):
+    pattern = r'^(Figure|Fig)\s*\d+[.:]'
+    return re.match(pattern, row) is not None
+
+def starts_with_table(row):
+    pattern = r'^(Table|Tab)\s*\d+[.:]'
+    return re.match(pattern, row) is not None
+
+def is_finished(s):
+    pattern = r'\.$'
+    return bool(re.search(pattern, s))
+
+def remove_consecutive_whitespaces(text):
+    # Use regular expression to replace consecutive whitespaces with a single space
+    cleaned_text = re.sub(r'\s+', ' ', text)
+    return cleaned_text
+
+def remove_spaces_around_parentheses(text):
+    # Remove spaces after opening parenthesis, bracket, or brace and before a word
+    text = re.sub(r'(?<=[\(\[\{])\s+(?=\w)', '', text)
+    # Remove spaces after a word and before closing parenthesis, bracket, or brace
+    text = re.sub(r'(?<=\w)\s+(?=[\)\]\}])', '', text)
+    return text
+
+def select_text_smaller(document, font_size=8):
+    # this is ok for we don't are not currently using the 
+    doc = copy.deepcopy(document)
+    selected_text = []
+
+    for paragraph in doc.paragraphs:
+        for run in paragraph.runs:
+            # Check if the font size is smaller than font_size points
+            if run.font.size and run.font.size.pt <= font_size:
+                run._element.getparent().remove(run._element)
+                # selected_text.append(run.text)
+                # if run.text != "":
+                #     print(run.text)
+    return doc
+
+def select_text_greater(document, font_size=13):
+    # this is ok for we don't are not currently using the
+    doc = copy.deepcopy(document)
+    selected_text = []
+
+    for paragraph in doc.paragraphs:
+        for run in paragraph.runs:
+            # Check if the font size is smaller than font_size points
+            if run.font.size and run.font.size.pt > font_size:
+                run._element.getparent().remove(run._element)
+                # selected_text.append(run.text)
+                # if run.text != "":
+                #     print(run.text)
+    return doc
+
+
+def merge_lowercase_with_hyphen(text):
+    # Define a regex pattern to match lowercase strings containing hyphens
+    pattern = r'\b[a-z]+-[a-z]+\b'
+
+    # Find all matches in the text
+    matches = re.findall(pattern, text)
+
+    # Replace each match with a merged string
+    for match in matches:
+        # Merge lowercase strings with hyphen
+        merged_string = match.replace("-", "")
+        # Replace the match with the merged string in the original text
+        text = text.replace(match, merged_string)
+
+    return text
+
+
+def delete_images_and_tables(document):
+    
+    # doc = document.copy()
+    doc = copy.deepcopy(document)
+    images_to_delete = []
+    tables_to_delete = []
+
+    for idx, paragraph in enumerate(doc.paragraphs):
+        # Check for images
+        for run in paragraph.runs:
+            if run._element.tag == '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}r':
+                for pic in run._element.iter('{http://schemas.openxmlformats.org/drawingml/2006/picture}pic'):
+                    images_to_delete.append((idx, pic))
+
+    # Remove images
+    for idx, pic in images_to_delete:
+        doc.paragraphs[idx].clear()
+
+    # Identify tables to delete
+    tables_to_delete = []
+    for idx, table in enumerate(doc.tables):
+        tables_to_delete.append(idx)
+
+    # Remove tables
+    for idx in reversed(tables_to_delete):
+        doc._element.body.remove(doc.tables[idx]._element)
+
+    return doc
+
+
+def text_to_sentences(text):
+    doc = nlp(text)
+    sentences = [sent.text.strip() for sent in doc.sents]
+    return sentences
+
+def correct_morphology(text):
+    matches = tool.check(text)
+    # select the matches to be applid to the text
+    for m in matches:
+        if m.ruleIssueType == "misspelling":
+            try:
+                m.replacements = [m.replacements[1]]
+            except IndexError:
+                continue
+        else:
+            matches.remove(m)
+    corrected_text = language_tool_python.utils.correct(text, matches)
+    return corrected_text
+
+def pipeline(document):
+
+    doc = delete_images_and_tables(document)
+    doc = select_text_smaller(doc)
+    doc = select_text_greater(doc)
+
+    attachments = []
+    show = False
+    data = []
+
+    last_data = ""
+    for p in doc.paragraphs:
+        if starts_with_table(p.text) or starts_with_figure(p.text):
+            attachments.append(p.text)
+            continue
+
+        if p.text.upper() == "REFERENCES":
+            show = False
+            continue
+        if p.text.upper() == "INTRODUCTION":
+            show = True
+            continue
+        if p.text.isnumeric():
+            continue
+        if p.text.upper() == p.text:
+            continue
+
+        if p.text != "" and show:
+            # print(p.text)
+            if is_finished(p.text):
+                last_data += (" " + p.text)
+                # print(last_data)
+                continuous_text = merge_lowercase_with_hyphen(last_data)
+                morph_text = correct_morphology(continuous_text)
+                clean_text = remove_consecutive_whitespaces(morph_text)
+                clean_text = remove_spaces_around_parentheses(clean_text)
+                clean_text = clean_text.strip()
+                clean_text = re.sub(r"\n+", " ", clean_text)
+                clean_sentences = clean_text.strip().replace(". ", ".<|>").split("<|>")
+
+                # clean_sentences = text_to_sentences(clean_text)
+
+                data.extend(clean_sentences)
+
+                last_data = ""
+            else:
+                last_data = p.text.replace("-\n", "")
+            # continue
+    data.extend(attachments)
+    return data
+    
+input_folder = Path("dataset_docx_ocr")
+
+input_files = input_folder.glob("*.docx")
+
+# dataset = []
+text_rels_pairs = []
+for f in input_files:
+    document = Document(f)
+    data = pipeline(document)
+    
+    for d in data:
+        rels = ner_extract(d)
+        row = {
+            "descriptor": f.stem,
+            "text": d,
+            "ner": rels
+        }
+        text_rels_pairs.append(row)
+        
+    # f_name = f.stem
+    # data = list(product([f_name], data))
+    # dataset.extend(data)
+    # print(pd.DataFrame(text_rels_pairs))
+
+df = pd.DataFrame(text_rels_pairs)
+# df["rels"] = df["1"].apply(ner_extract)
+df.to_csv("doc_clean_data_no_line_break.csv", index=None)
+
+
+# def process_document(f):
+#     document = Document(f)
+#     data = pipeline(document)  # Assuming you have defined your pipeline function
+#     f_name = f.stem
+#     data = list(product([f_name], data))
+#     return data
+
+# # Define the number of processes
+# num_processes = 8  # Adjust as needed
+
+# # Create a Pool of processes
+# with Pool(num_processes) as pool:
+#     # Map the process_document function to each input file
+#     results = pool.map(process_document, input_files)
+
+# # Flatten the results list
+# dataset = [item for sublist in results for item in sublist]
+
+# # Create a DataFrame from the dataset
+# df = pd.DataFrame(dataset)
+
+# # Save the DataFrame to CSV
+# df.to_csv("doc_clean_parallel_data.csv", index=None)
---
+++ kg_llama_index.py
@@ -0,0 +1,125 @@
+
+import os
+import logging
+import sys
+import pandas as pd
+logging.basicConfig(
+    stream=sys.stdout, level=logging.INFO
+)  
+# from langchain.docstore.document import Document
+from llama_index import Document
+from llama_index import (
+    KnowledgeGraphIndex,
+    LLMPredictor,
+    ServiceContext,
+    SimpleDirectoryReader,
+)
+from llama_index.storage.storage_context import StorageContext
+from llama_index.query_engine import KnowledgeGraphQueryEngine
+
+from llama_index.graph_stores import NebulaGraphStore
+from llama_index.graph_stores import Neo4jGraphStore
+
+from llama_index.llms import Ollama, OpenAI
+
+from llama_index.embeddings import OllamaEmbedding
+
+
+# llm = OpenAI(temperature=0, model="gpt-3.5-turbo-16k")
+#llm = Ollama(temperature=0, model="mistral")
+
+llm = Ollama(model="mistral", temperature=0.0, context_window=4900, request_timeout=300)
+embedding_llm = OllamaEmbedding(model_name="mistral")
+
+service_context = ServiceContext.from_defaults(llm=llm, embed_model=embedding_llm)
+
+from llama_index import SimpleDirectoryReader
+
+os.environ["NEBULA_USER"] = "root"
+os.environ["NEBULA_PASSWORD"] = "nebula"
+os.environ[
+    "NEBULA_ADDRESS"
+] = "127.0.0.1:9669"
+
+
+# reader = SimpleDirectoryReader(input_dir="../data/knowledge graphs/rebel_llamaindex/wiki/")
+# documents = reader.load_data()
+
+df = pd.read_csv("doc_clean_data.csv")
+
+docs = df.groupby("0")
+documents = []
+for fname, doc in docs:
+    page_count = 0
+    block = ""
+    for i, row in doc.iterrows():
+        content = row["1"]
+        # print(fname, i, len(content))
+
+        if len(block) + len(content) <= 4000:
+            # page_count += len(content)
+            block += (" " + content)
+            page_content = block
+        else: 
+            # print("page_content: ", fname, len(page_content))
+            # page_count = 0
+            block = content
+        d = Document(
+            text = page_content,
+            # source=fname,
+            # get_doc_id=fname,
+        )
+        documents.append(d)
+    page_content = " ".join(doc["1"])
+    # print(fname, len(page_content))
+
+
+space_name = "covid_relationships"
+edge_types, rel_prop_names = ["relationship"], [
+    "relationship"
+]  
+tags = ["entity"]
+
+# graph_store = NebulaGraphStore(
+#     space_name=space_name,
+#     edge_types=edge_types,
+#     rel_prop_names=rel_prop_names,
+#     tags=tags,
+# )
+
+
+# username = "neo4j"
+# password = "9eGGLTE9W13bttviLjMmQRpzaxYzWSmhiW64CrGi40w"
+# url = "neo4j+s://4feea599.databases.neo4j.io"
+# database = "neo4j"
+
+username = "neo4j"
+password = "password"
+url = "neo4j://127.0.0.1:7687"
+database = "neo4j"
+
+graph_store = Neo4jGraphStore(
+    username=username, password=password, url=url, database=database
+)
+storage_context = StorageContext.from_defaults(graph_store=graph_store)
+
+
+kg_index = KnowledgeGraphIndex.from_documents(
+    documents,
+    storage_context=storage_context,
+    max_triplets_per_chunk=5,
+    service_context=service_context,
+    space_name=space_name,
+    edge_types=edge_types,
+    rel_prop_names=rel_prop_names,
+    tags=tags,
+    include_embeddings=True,
+)
+
+
+query_engine = KnowledgeGraphQueryEngine(
+    storage_context=storage_context,
+    service_context=service_context,
+    llm=llm,
+    verbose=True,
+)
