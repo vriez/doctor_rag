bb4c309b9aaba5908cbc437cd0861338e612d3b7
Vítor Eulálio Reis <reis.vitore@gmail.com>
2024-04-04 22:02:28 -0300

    wip

--- RAG_bulk.py
+++ RAG_bulk.py
@@ -77,7 +77,7 @@ MAX_TRIPLETS = 320
 # # bolt+s://c8ac89364ecd0581662c26ca8fcd869e.neo4jsandbox.com:7687
 
 
-# # 2048 multithreaded index
+# # 2048 multithreaded
 # username = "neo4j"
 # password = "REDACTED_NEO4J_PASSWORD"
 # url = "bolt://REDACTED_IP:7687"
